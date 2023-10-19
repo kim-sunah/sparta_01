ff065c543f202097dff51581c71e46eca5165b6c
sunah <1004_sunah@naver.com>
2023-10-19 12:19:16 +0900

    숙제

---
+++ index.js
@@ -0,0 +1,43 @@
+const cardList = document.getElementById("cardList")
+const searchFrom = document.getElementById("search");
+const searchInput = searchFrom.querySelector('input');
+const movieCard = document.querySelector(".movieCard")
+searchFrom.addEventListener("submit", search)
+
+var movieClick = function(event){
+alert(event)
+}
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMzliNjIwOTFmMzY0Y2M4MzczMGExMzU3ZWM1YjE3ZCIsInN1YiI6IjY1MmY3NTcyMzU4ZGE3NWI1ZDAwODcyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j6JDptMTCwZT8Gkr2PbQ2rWV5r85H1fKNwS4iF1_o3U'
+    }
+};
+
+
+
+function search(event) {
+    if (event != null) {
+        event.preventDefault();
+    }
+    const searchWord = (searchInput.value).toUpperCase();
+    cardList.innerHTML = "";
+    fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(response => {
+            response.results.filter(element => {
+                if (element.original_title.toUpperCase().includes(searchWord)) { return true }
+            }
+            ).map(element => {
+                cardList.innerHTML += `
+                <div class = "movieCard" id = "${element.id}" onclick = "movieClick(${element.id})" >
+                    <img class = "movieImg" src = "https://image.tmdb.org/t/p/w500/${element.poster_path}"/>
+                    <h3 class = "movieTtile">${element.original_title}</h3>
+                    <p class = "movieTxt" >${element.overview}</p>
+                </div>`
+            });
+        })
+        .catch(err => console.error(err));
+}
+search()
